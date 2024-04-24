0546a5c774e2a938433b9b0a2da9f88a0d6b724c
seongil <68831585+1di0t@users.noreply.github.com>
2024-04-24 14:28:51 +0000

    Update memory_07.cpp

--- memory_07.cpp
+++ memory_07.cpp
@@ -1,15 +1,15 @@
-//#include <iostream>
-//using namespace std;
-//
-//
-//int main() {
-//
-//	string stoooring("Pikachu ThunderBoltttttttttttttttttt");//constructer
-//	string str(stoooring);//copy constructer
-//	cout << str << "\n";
-//	cout << stoooring << "\n";
-//	cout << stoooring.size() << "\n";
-//	cout << stoooring.capacity() << "\n";
-//	return 0;
-//	
-//}
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+	string stoooring("Pikachu ThunderBoltttttttttttttttttt");//constructer
+	string str(stoooring);//copy constructer
+	cout << str << "\n";
+	cout << stoooring << "\n";
+	cout << stoooring.size() << "\n";
+	cout << stoooring.capacity() << "\n";
+	return 0;
+	
+}
