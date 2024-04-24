9b2cee02b279913cac53ad84144995e829c7fde4
seongil <68831585+1di0t@users.noreply.github.com>
2024-04-24 14:58:02 +0000

    Update pointer_02.cpp

--- pointer_02.cpp
+++ pointer_02.cpp
@@ -1,14 +1,14 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//	int i = 7;
-//	double d = 3.14;
-//	int* pi = &i;
-//	void* pv;//operating method : get address, developer remembered the type and calculate end point => read starting address to end address
-//	pv = &d;
-//	
-//	cout << *pi << " " << i << "\n";
-//	cout << *(double*)pv << "\n";//to use void pointer must remember pointing type//In this case double
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i = 7;
+	double d = 3.14;
+	int* pi = &i;
+	void* pv;//operating method : get address, developer remembered the type and calculate end point => read starting address to end address
+	pv = &d;
+	
+	cout << *pi << " " << i << "\n";
+	cout << *(double*)pv << "\n";//to use void pointer must remember pointing type//In this case double
+	return 0;
+}
